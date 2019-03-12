1cf98b7eb78de0aeab29cec410e9fdf8d82ecd36
Manuel Pita <mpita1984@gmail.com>
2019-03-12 23:51:26 +0100

    unittest model particular

---
+++ cride/circles/tests/test_invitations.py
@@ -0,0 +1,61 @@
+"""Invitations tests."""
+
+# Django
+from django.test import TestCase
+
+# Model
+from cride.circles.models import Circle, Invitation
+from cride.users.models import User
+
+
+class InvitationsManagerTestCase(TestCase):
+    """Invitations manager test case."""
+
+    def setUp(self):
+        """Test case setup."""
+        self.user = User.objects.create(
+            first_name='Manuel',
+            last_name='Pita',
+            email='mpita1984@gmail.com',
+            username='mpita',
+            password='admin123'
+        )
+        self.circle = Circle.objects.create(
+            name='Test Django',
+            slug_name='test_django',
+            about='data of test django',
+            verified=True
+        )
+
+    def test_code_generation(self):
+        """Random codes should be generated automatically."""
+        invitation = Invitation.objects.create(
+            issued_by=self.user,
+            circle=self.circle
+        )
+        self.assertIsNotNone(invitation.code)
+
+    def test_code_usage(self):
+        code = 'helloword'
+        invitation = Invitation.objects.create(
+            issued_by=self.user,
+            circle=self.circle,
+            code=code
+        )
+        self.assertEqual(invitation.code, code)
+
+    def test_code_generation_if_duplicated(self):
+        """If given code is not unique, a new one must be generated."""
+        code = Invitation.objects.create(
+            issued_by=self.user,
+            circle=self.circle
+        ).code
+
+        # Create another invitation with the past code
+        invitation = Invitation.objects.create(
+            issued_by=self.user,
+            circle=self.circle,
+            code=code
+        )
+
+        self.assertNotEqual(code, invitation.code)
